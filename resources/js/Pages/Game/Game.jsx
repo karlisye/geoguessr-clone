8a58a7c501fe35e4b93a8f511664ce9502db0e91
karlisye <voiconokskarls@gmail.com>
2025-12-07 14:28:07 +0200

    add map

--- resources/js/Pages/Game/Game.jsx
+++ resources/js/Pages/Game/Game.jsx
@@ -1,11 +1,38 @@
 import { Link } from '@inertiajs/react';
-import React from 'react'
+import React, { useState } from 'react'
+
+const MAP_WIDTH = 800;
+const MAP_HEIGHT = 400;
 
 const Game = () => {
-  return (
-    <div className='h-screen relative'>
+  const [isHovered, setIsHovered] = useState(false);
 
-        <Link className='py-2 px-6 font-bold text-lg border-2 border-red-300 bg-red-500 rounded-xl absolute left-1/2 bottom-5 text-white transform -translate-x-1/2' href='/'>Stop</Link>
+  const handleMouseEnter = (e) => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = (e) => {
+    setIsHovered(false)
+  }
+
+  return (
+    <div className='h-screen relative w-screen object-cover'>
+      <Link className='py-2 px-6 font-bold text-lg border-2 border-red-300 bg-red-500 rounded-xl absolute m-5 left-0 top-0 text-white z-10' href='/'>Stop</Link>
+      
+      <img className='absolute w-screen h-screen object-cover' src="images/location1.png" />
+
+      <div className='absolute border-2 bottom-0 right-0 m-5'>
+        <img 
+          className='transition-discrete transition-all duration-300'
+          src="images/maps/map1.png"
+          style={{
+            width: isHovered ? MAP_WIDTH : MAP_WIDTH / 2,
+            height: isHovered ? MAP_HEIGHT : MAP_HEIGHT / 2,
+          }}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        />
+      </div>
     </div>
   )
 }
