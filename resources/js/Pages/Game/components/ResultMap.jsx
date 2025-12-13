56c7c19e588a94e9dbd50a06d5a3149999f5f13e
Kārlis <voiconokskarls@gmail.com>
2025-12-14 01:02:28 +0200

    add simple zoom and drag to map

--- resources/js/Pages/Game/components/ResultMap.jsx
+++ resources/js/Pages/Game/components/ResultMap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Marker from './Marker'
 import { useGame } from '../../../contexts/GameContext'
 import { calculateXY } from '../services/mapCalculations';
@@ -8,6 +8,11 @@ const ResultMap = () => {
   const { markerLocation, setMarkerLocation, realLocation, setRealLocation, locations, locationIndex, latLon } = useGame();
   const [imgWidth, setImgWidth] = useState(0);
   const [imgHeight, setImgHeight] = useState(0);
+
+  const [scale, setScale] = useState(1);
+  const [offset, setOffset] = useState({ x:0, y:0 });
+  const [dragging, setDragging] = useState(false);
+  const dragStart = useRef({ x:0, y:0});
 
   useEffect(() => {
     setImgWidth(window.innerWidth);
@@ -27,23 +32,75 @@ const ResultMap = () => {
     return { percentX, percentY, x, y };
   }
 
+  const zoomIn  = () => setScale(prev => Math.min(prev + 0.2, 5));
+  const zoomOut  = () => setScale(prev => Math.max(prev - 0.2, 1));
+  const resetZoom = () => {
+    setScale(1);
+    setOffset({ x:0, y:0 })
+  }
+
+  const onMouseDown = (e) => {
+    setDragging(true);
+    dragStart.current = {
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y,
+    };
+  }
+
+  const onMouseMove = (e) => {
+    if (!dragging) return;
+
+    setOffset({
+      x: e.clientX - dragStart.current.x,
+      y: e.clientY - dragStart.current.y,
+    });
+  }
+
+  const onMouseUp = () => setDragging(false);
+
+
   return (
     <>
     {realLocation ? (
-      <div className='relative'
+      <>
+      <div className='relative overflow-hidden'
         style={{
           width: imgWidth,
-          height: imgHeight
+          height: imgHeight,
         }}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onMouseMove={onMouseMove}
       >
-        <img className='absolute w-full' src={`images/maps/map1.png`} />
+        <div
+          className='absolute top-0 left-0'
+          style={{
+            width: imgWidth,
+            height: imgHeight,
+            transform: `translate(${offset.x}px, ${offset.y}px) scale(${scale})`,
+            transformOrigin: '0 0',
+        }}
+        >
+          <img 
+            className='absolute w-full h-full pointer-events-none' 
+            src={`images/maps/map1.png`} 
+          />
 
-        <Marker location={markerLocation} color='red' />
+          <Marker location={markerLocation} color='red' />
 
-        <Marker location={realLocation} color='green' />
+          <Marker location={realLocation} color='green' />
 
-        <Line start={markerLocation} end={realLocation} />
+          <Line start={markerLocation} end={realLocation} />
+        </div>
+
       </div>
+
+      <div className='fixed bottom-0 right-0 m-2 flex gap-2'>
+        <button onClick={zoomIn} className='border p-2 text-xl font-bold rounded-md bg-white border-stone-200 hover:cursor-pointer hover:bg-stone-100'>+</button>
+        <button onClick={zoomOut} className='border p-2 text-xl font-bold rounded-md bg-white border-stone-200 hover:cursor-pointer hover:bg-stone-100'>-</button>
+        <button onClick={resetZoom} className='border p-2 text-xl font-bold rounded-md bg-white border-stone-200 hover:cursor-pointer hover:bg-stone-100'>reset</button>
+      </div>
+      </>
       ) : (
         <p>Loading</p>
       )}
